ff0aa9dcd457686bcd954cd61a1bd8758bf4ab42
Boumadane Abdelmoumene <fa_boumadane@esi.dz>
2020-08-26 01:02:12 +0200

    Added  export wordcloud as PNG

--- easy_nlp/data_visualisation/data_visualisation.py
+++ easy_nlp/data_visualisation/data_visualisation.py
@@ -35,7 +35,28 @@ class WordCloudMaker:
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis("off")
         plt.show()
+ 
+    def fit_export(sentences):
+        """
+        display wordcloud of a sentence 
+        :param sentence : list of sentences   
+        :param        
+        :returns: true if tweet has image mention False if it's not 
+        """
+        sentences = [word_tokenize(sentence) for sentence in sentences]
+        stp_words = stopwords.words('french')
 
+        sentences_cleaned = [
+            word for sentence in sentences for word in sentence if word not in stp_words]
+        sentences_cleaned = ' '.join(sentences_cleaned)
+
+        wordcloud = WordCloud(max_font_size=50, max_words=100,
+                              background_color="white").generate(sentences_cleaned)
+
+        # Display the generated image:
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+        plt.savefig('tmp.png')
 
 class ReportGenerator:
     def __init__(self):
